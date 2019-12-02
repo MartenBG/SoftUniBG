38154eee12d808436727251e66039af34919a290
MartenBG <martinezibg@gmail.com>
2019-12-02 17:53:46 +0200

    02

---
+++ Fundamentals Final Exam/Final Exam Preparation - 24 July 2019/01. Concert.js
@@ -0,0 +1,18 @@
+function solve(arr){
+
+
+
+
+}
+
+solve([
+    'Play; The Beatles; 2584',
+    'Add; The Beatles; John Lennon, Paul McCartney, George Harrison, Ringo Starr',
+    'Add; Eagles; Glenn Frey, Don Henley, Bernie Leadon, Randy Meisner',
+    'Play; Eagles; 1869',
+    'Add; The Rolling Stones; Brian Jones, Mick Jagger, Keith Richards',
+    'Add; The Rolling Stones; Brian Jones, Mick Jagger, Keith Richards, Bill Wyman, Charlie Watts, Ian Stewart',
+    'Play; The Rolling Stones; 4239',
+    'start of concert',
+    'The Rolling Stones'
+  ]);
