c7e0a42a3ebede264f3f9854bdc2da53be708ebd
MartenBG <martinezibg@gmail.com>
2020-01-15 17:17:15 +0200

    15.01.2020

---
+++ temp/03. Same Numbers.js
@@ -0,0 +1,25 @@
+function solve(n) {
+
+    let compare = String(n);
+    let sum = compare.split('').reduce((a, b) => Number(a) + Number(b));
+
+    for (let i = 1; i <= compare.length - 1; i++) {
+        var isTheSame = true;
+
+        if (compare[0] !== compare[i]) {
+
+            isTheSame = false;
+            break;
+        }
+    }
+    if (isTheSame) {
+        console.log('true');
+        console.log(sum);
+    } else {
+        console.log('false');
+        console.log(sum);
+    }
+
+}
+
+solve(2222222);
