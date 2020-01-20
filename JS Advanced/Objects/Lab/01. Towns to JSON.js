583cec3e2ea4bef7c5c56122c183d255b68b1e2f
MartenBG <martinezibg@gmail.com>
2020-01-20 22:24:37 +0200

    20.01.2020

---
+++ JS Advanced/Objects/Lab/01. Towns to JSON.js
@@ -0,0 +1,25 @@
+function solve(arr) {
+
+
+    
+
+
+    for (const input of arr) {
+
+
+        
+    }
+
+    arr = arr.split(' | ');
+    console.log(arr);
+    
+
+
+
+
+}
+
+solve(['| Town | Latitude | Longitude |',
+    '| Sofia | 42.696552 | 23.32601 |',
+    '| Beijing | 39.913818 | 116.363625 |']
+);
